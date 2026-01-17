511cfb02e6371b2da0a15e2717139b18e5bcf5a0
shuosha <shashuo0104@gmail.com>
2026-01-17 04:36:40 -0500

    add align, change align rew scale

--- source/isaaclab_tasks/isaaclab_tasks/direct/factory_xarm/factory_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/direct/factory_xarm/factory_env_cfg.py
@@ -168,7 +168,7 @@ class EnvOptionsCfg:
 
     task_success_reward_scale = 500.0
     termination_reward_scale = 50.0
-    xy_align_reward_scale = 1.0
+    xy_align_reward_scale = 5.0
     action_norm_reward_scale = 1.0
     tilt_penalty_reward_scale = 2.0
     force_penalty_reward_scale = 1.0
