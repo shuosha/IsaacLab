95f58e469023abbcbd81f6f5491ce96f35ab14fc
shuosha <shashuo0104@gmail.com>
2026-01-20 20:01:16 -0500

    add interpolation to nn buffer, change hor 5-15, tuned rew scale, clean up get_reward() code

--- scripts/train_factory.sh
+++ scripts/train_factory.sh
@@ -2,15 +2,15 @@ time="$(date +%Y-%m-%d_%H-%M-%S)"
 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-PegInsert-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_peginsert_constr_nn_term_align  --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_peginsert_constr_nn_term_align  --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_peginsert_constr_nn_tune_rew_v2   --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_peginsert_constr_nn_tune_rew_v2   --headless --base nn
 
 CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
     --task Isaac-Factory-Xarm-GearMesh-Residual --num_envs 128 \
-    --track --wandb-project-name FactoryXarm --wandb-name ${time}_gearmesh_constr_nn_term_align --wandb-entity ss7050-columbia \
-    agent.params.config.full_experiment_name=${time}_gearmesh_constr_nn_term_align --headless --base nn
+    --track --wandb-project-name FactoryXarm --wandb-name ${time}_gearmesh_constr_nn_tune_rew_v2 --wandb-entity ss7050-columbia \
+    agent.params.config.full_experiment_name=${time}_gearmesh_constr_nn_tune_rew_v2 --headless --base nn
 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-NutThread-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_nutthread_constr_nn_term_align  --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_nutthread_constr_nn_term_align  --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_nutthread_constr_nn_tune_rew_v2   --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_nutthread_constr_nn_tune_rew_v2   --headless --base nn
