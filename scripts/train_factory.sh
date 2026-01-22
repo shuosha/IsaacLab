1b30724c29692e885e2ff08087ba9fc18eac6cdf
shuosha <shashuo0104@gmail.com>
2026-01-22 02:04:21 -0500

    add residual smoothing penalty

--- scripts/train_factory.sh
+++ scripts/train_factory.sh
@@ -1,16 +1,16 @@
 time="$(date +%Y-%m-%d_%H-%M-%S)"
 
-# CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
-#     --task Isaac-Factory-Xarm-PegInsert-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_peginsert_constr_nn_new_data_v5   --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_peginsert_constr_nn_new_data_v5   --headless --base nn
+CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
+    --task Isaac-Factory-Xarm-PegInsert-Residual --num_envs 128 \
+    --track --wandb-project-name FactoryXarm --wandb-name ${time}_peginsert_constr_nn_new_data_v6   --wandb-entity ss7050-columbia \
+    agent.params.config.full_experiment_name=${time}_peginsert_constr_nn_new_data_v6   --headless --base nn
 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-GearMesh-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_gearmesh_constr_nn_new_data_v5 --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_gearmesh_constr_nn_new_data_v5 --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_gearmesh_constr_nn_new_data_v6 --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_gearmesh_constr_nn_new_data_v6 --headless --base nn
 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-NutThread-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_nutthread_constr_nn_new_data_v5   --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_nutthread_constr_nn_new_data_v5   --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_nutthread_constr_nn_new_data_v6   --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_nutthread_constr_nn_new_data_v6   --headless --base nn
