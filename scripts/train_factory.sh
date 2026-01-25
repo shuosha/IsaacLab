0eb0dfd3dfb17af0f6022559e6ea0cc125f6c945
shuosha <shashuo0104@gmail.com>
2026-01-24 19:09:59 -0500

    v11: rew s100 t50 train all pilots

--- scripts/train_factory.sh
+++ scripts/train_factory.sh
@@ -1,16 +1,17 @@
 time="$(date +%Y-%m-%d_%H-%M-%S)"
-
+# export LD_PRELOAD=/home/shuo/projects/IsaacLab/env_isaac/lib/python3.11/site-packages/scikit_learn.libs/libgomp-e985bcbb.so.1.0.0:${LD_PRELOAD}
+# 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-PegInsert-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_peginsert_train_nn_v9   --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_peginsert_train_nn_v9 --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_peginsert_train_nn_v11   --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_peginsert_train_nn_v11 --headless --base nn
 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-GearMesh-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_gearmesh_train_nn_v9 --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_gearmesh_train_nn_v9 --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_gearmesh_train_nn_v11 --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_gearmesh_train_nn_v11 --headless --base nn
 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-NutThread-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_nutthread_train_nn_v9   --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_nutthread_train_nn_v9   --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_nutthread_train_bc_v11   --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_nutthread_train_bc_v11   --headless --base bc
