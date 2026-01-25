6ebfb4f1f22c99574646fce131e64b7f72464a76
shuosha <shashuo0104@gmail.com>
2026-01-25 02:31:17 -0500

    v12.5 action norm

--- scripts/train_factory.sh
+++ scripts/train_factory.sh
@@ -1,17 +1,17 @@
 time="$(date +%Y-%m-%d_%H-%M-%S)"
-# export LD_PRELOAD=/home/shuo/projects/IsaacLab/env_isaac/lib/python3.11/site-packages/scikit_learn.libs/libgomp-e985bcbb.so.1.0.0:${LD_PRELOAD}
+export LD_PRELOAD=/home/shuo/projects/IsaacLab/env_isaac/lib/python3.11/site-packages/scikit_learn.libs/libgomp-e985bcbb.so.1.0.0:${LD_PRELOAD}
 # 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-PegInsert-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_peginsert_train_nn_v12   --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_peginsert_train_nn_v12 --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_peginsert_train_noisy_nn_v12.5   --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_peginsert_train_noisy_nn_v12.5 --headless --base noisy_nn
 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-GearMesh-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_gearmesh_train_nn_v12 --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_gearmesh_train_nn_v12 --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_gearmesh_train_noisy_nn_v12.5 --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_gearmesh_train_noisy_nn_v12.5 --headless --base noisy_nn
 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-NutThread-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_nutthread_train_nn_v12   --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_nutthread_train_nn_v12   --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_nutthread_train_noisy_nn_v12.5   --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_nutthread_train_noisy_nn_v12.5 --base noisy_nn
