2b176d6111d76bb76c6af1fa3fa4403554c30689
shuosha <shashuo0104@gmail.com>
2026-01-24 23:05:24 -0500

    v12 increase residual scale, increase sparse reward scale

--- scripts/train_factory.sh
+++ scripts/train_factory.sh
@@ -3,15 +3,15 @@ time="$(date +%Y-%m-%d_%H-%M-%S)"
 # 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-PegInsert-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_peginsert_train_nn_v11   --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_peginsert_train_nn_v11 --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_peginsert_train_nn_v12   --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_peginsert_train_nn_v12 --headless --base nn
 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-GearMesh-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_gearmesh_train_nn_v11 --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_gearmesh_train_nn_v11 --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_gearmesh_train_nn_v12 --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_gearmesh_train_nn_v12 --headless --base nn
 
 # CUDA_VISIBLE_DEVICES=0 python scripts/reinforcement_learning/rl_games/train.py \
 #     --task Isaac-Factory-Xarm-NutThread-Residual --num_envs 128 \
-#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_nutthread_train_nn_v11.1   --wandb-entity ss7050-columbia \
-#     agent.params.config.full_experiment_name=${time}_nutthread_train_nn_v11.1   --headless --base nn
+#     --track --wandb-project-name FactoryXarm --wandb-name ${time}_nutthread_train_nn_v12   --wandb-entity ss7050-columbia \
+#     agent.params.config.full_experiment_name=${time}_nutthread_train_nn_v12   --headless --base nn
